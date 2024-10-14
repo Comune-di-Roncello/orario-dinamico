dc05430c693a446309e162bd7d3406949bcc7470
icovada <federico@tabbo.it>
2024-10-14 19:08:40 +0200

    Find first open slot past current time

--- splash/webpage/views.py
+++ splash/webpage/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.db.models import OuterRef, Subquery
+
 
 from . import models
 
@@ -7,6 +9,7 @@ from datetime import datetime as dt
 # Create your views here.
 def home(request):
     #Lunedì 0 domenica 6
+    day_of_week = 1
     day_of_week = dt.today().weekday()
     time_now = dt.now().time()
     now = time_now.hour*60+time_now.minute
@@ -15,55 +18,86 @@ def home(request):
         day_of_week=day_of_week
     )
 
-    open_today = []
+    open_today = {}
     for apertura in aperti:
         open_time = apertura.time_start.hour*60+apertura.time_start.minute
         close_time = apertura.time_end.hour*60+apertura.time_end.minute
         
-        this_open = {
-            "name": apertura.fkufficio.name,
-        }
 
         if now < open_time:
             # Non ancora aperto
             minutes_to_open = open_time-now
 
             if minutes_to_open > 15:
-                this_open.update({
-                    "badge": "primary",
-                    "text": f"Apre alle {apertura.time_start.strftime('%H:%M')}"
+                open_today.update({
+                    apertura.fkufficio.name: {
+                        "badge": "primary",
+                        "text": f"Apre alle {apertura.time_start.strftime('%H:%M')}"
+                    }
                 })
             else:
-                this_open.update({
-                    "badge": "info",
-                    "text": f"Apre tra {minutes_to_open} minuti"
+                open_today.update({
+                    apertura.fkufficio.name: {
+                       "badge": "info",
+                        "text": f"Apre tra {minutes_to_open} minuti"
+                    }
                 })
-
+            
         elif now > close_time:
             # Già chiuso
             if now-close_time > 30:
                 continue
 
-            this_open.update({
-                "badge": "danger",
-                "text": "Chiuso"
+            open_today.update({
+                apertura.fkufficio.name: {
+                    "badge": "danger",
+                    "text": "Chiuso"
+                }
             })
-            
+
         else:
             # Aperto
             minutes_to_close = close_time-now
             if minutes_to_close < 15:
-                this_open.update({
-                    "badge": "warning",
-                    "text": f"Chiude tra {minutes_to_close} minuti"
+                open_today.update({
+                    apertura.fkufficio.name: {
+                        "badge": "warning",
+                        "text": f"Chiude tra {minutes_to_close} minuti"
+                    }
                 })
+
             else:
-                this_open.update({
-                    "badge": "success",
-                    "text": "Aperto"
+                open_today.update({
+                    apertura.fkufficio.name: {
+                        "badge": "success",
+                        "text": "Aperto"
+                    }
                 })
 
-        open_today.append(this_open)
+    open_this_week = models.Orario.objects.filter(pkid__in=Subquery(
+        models.Ufficio.objects.annotate(
+            first_open=Subquery(
+                models.Orario.objects.filter(
+                    fkufficio=OuterRef('pkid'), 
+                    day_of_week__gt=day_of_week
+                ).order_by('day_of_week', 'time_start').values('pkid')[:1]
+            )
+        ).values('first_open')
+    ))
+    
+    open_next_week = models.Orario.objects.filter(pkid__in=Subquery(
+        models.Ufficio.objects.annotate(
+            first_open=Subquery(
+                models.Orario.objects.filter(
+                    fkufficio=OuterRef('pkid'), 
+                    day_of_week__lt=day_of_week
+                ).order_by('day_of_week', 'time_start').values('pkid')[:1]
+            )
+        ).values('first_open')
+    ))
 
+    filtered_next_week = open_next_week.exclude(fkufficio__pkid__in=Subquery(open_this_week.values('fkufficio__pkid')))
 
-    return render(request, "webpage/index.html", context = {"open_today": open_today})
+    output_openings = filtered_next_week.union(open_this_week, all=True)
+
+    return render(request, "webpage/index.html", context = {"open_today": open_today, "next_open": output_openings})
