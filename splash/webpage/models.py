4e34d23a9ba4fd6c6e22313b0ecd54815b7b32ab
icovada <federico@tabbo.it>
2024-10-14 17:24:22 +0200

    Create models and register them

--- splash/webpage/models.py
+++ splash/webpage/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from uuid import uuid4
+
 
 # Create your models here.
+class Ufficio(models.Model):
+    pkid = models.UUIDField(primary_key=True, default=uuid4)
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+class Orario(models.Model):
+    pkid = models.UUIDField(primary_key=True, default=uuid4)
+    fkufficio = models.ForeignKey(to=Ufficio, on_delete=models.CASCADE)
+    time_start = models.TimeField()
+    time_end = models.TimeField()
+    day_of_week = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.fkufficio.name}: {self.time_start}-{self.time_end}"
