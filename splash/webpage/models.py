ac10326d9e382860903f258b6fee688f134595ae
icovada <federico@tabbo.it>
2024-10-14 18:57:51 +0200

    Add related name

--- splash/webpage/models.py
+++ splash/webpage/models.py
@@ -12,10 +12,10 @@ class Ufficio(models.Model):
 
 class Orario(models.Model):
     pkid = models.UUIDField(primary_key=True, default=uuid4)
-    fkufficio = models.ForeignKey(to=Ufficio, on_delete=models.CASCADE)
+    fkufficio = models.ForeignKey(to=Ufficio, on_delete=models.CASCADE, related_name='orario')
     time_start = models.TimeField()
     time_end = models.TimeField()
     day_of_week = models.IntegerField()
 
     def __str__(self):
-        return f"{self.fkufficio.name}: {self.time_start}-{self.time_end}"
+        return f"{self.fkufficio.name}: {self.time_start}-{self.time_end} gg {self.day_of_week}"
