a537b76754d31fd39c9b3c6b5c1f8f7e59c0cc57
Federico Tabbò <federico@tabbo.it>
2024-10-28 22:18:58 +0000

    Calculate badges in-place

--- od-frontend/src/stores/counter.ts
+++ od-frontend/src/stores/counter.ts
@@ -1,5 +1,6 @@
 import { defineStore } from 'pinia'
 import { type ButtonVariant } from 'bootstrap-vue-next'
+import { DateTime } from 'luxon'
 
 export interface OutputEvent {
   name: string
@@ -7,34 +8,29 @@ export interface OutputEvent {
   status: ButtonVariant | null
 }
 
-export class Event {
-  id!: string
-  createdDateTime!: string
-  lastModifiedDateTime!: string
-  originalStartTimeZone!: string
-  originalEndTimeZone!: string
-  subject!: string
-  bodyPreview!: string
-  isCancelled!: boolean
-  showAs!: string
-  type!: string
-  occurrenceId!: string
-  body!: Body
-  start: number
-  end: number
-  recurrence!: string
-  attendees!: Array<string>
-
-  constructor(start: string, end: string) {
-    this.start = Date.parse(start)
-    this.end = Date.parse(end)
-  }
+export interface Event {
+  id: string
+  createdDateTime: string
+  lastModifiedDateTime: string
+  originalStartTimeZone: string
+  originalEndTimeZone: string
+  subject: string
+  bodyPreview: string
+  isCancelled: boolean
+  showAs: string
+  type: string
+  occurrenceId: string
+  body: Body
+  start: string
+  end: string
+  recurrence: string
+  attendees: Array<string>
 }
 
 export const openOffices = defineStore('openOffices', {
   state: () => ({
     today: [] as OutputEvent[],
-    later: [] as OutputEvent[]
+    later: [] as OutputEvent[],
   }),
   actions: {
     getJson() {
@@ -45,29 +41,69 @@ export const openOffices = defineStore('openOffices', {
         .then(data => {
           this.today = []
           data.today.forEach((element: Event) => {
-            this.today.push(this.calculateEvent(element))
-          });
+            const outevent = this.calculateEvent(element)
+            if (outevent != null) {
+              this.today.push(outevent)
+            }
+          })
 
           this.later = []
           data.later.forEach((element: Event) => {
-            this.later.push(this.calculateEvent(element))
-          });
-
+            const outevent = this.calculateEvent(element)
+            if (outevent != null) {
+              this.later.push(outevent)
+            }
+          })
         })
         .catch(error => {
           console.log(error)
           return
         })
-
     },
-    calculateEvent(event: Event){
+    calculateEvent(event: Event) {
+      const begin = DateTime.fromISO(event.start, { zone: 'UTC' }).setZone(
+        'Europe/Rome',
+      )
+      const end = DateTime.fromISO(event.end, { zone: 'UTC' }).setZone(
+        'Europe/Rome',
+      )
+      const now = DateTime.now()
+      let status = 'info'
+      let text = ''
+
+      if (begin > now.endOf('day')) {
+        text = begin.setLocale('it-IT').toFormat('ccc d LLL, H:mm')
+      } else {
+        if (end.plus({ minutes: 10 }) < now) {
+          //Already over by over 10 min
+          return null
+        } else if (end < now) {
+          //Over
+          status = 'danger'
+          text = 'Chiuso'
+        } else if (end.minus({ minutes: 15 }) < now) {
+          // About to end
+          status = 'warning'
+          text = 'Chiude tra ' + end.diff(now).toFormat('m') + ' minuti'
+        } else if (begin < now) {
+          //open
+          status = 'success'
+          text = 'Aperto fino alle ' + end.toFormat('H:mm')
+        } else if (begin.minus({ minutes: 15 }) < now) {
+          // Hasn't started yet
+          status = 'primary'
+          text = 'Apre tra ' + begin.diff(now).toFormat('m:ss') + ' minuti'
+        } else {
+          status = 'info'
+          text = 'Apre alle ' + begin.toFormat('H:mm')
+        }
+      }
+
       return {
-        name: "antani",
-        text: "scappellamento",
-        status: "success"
-
+        name: event.subject,
+        text: text,
+        status: status,
       } as OutputEvent
-
-    }
+    },
   },
 })
