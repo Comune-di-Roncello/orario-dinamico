8af4fc8010d3a9c8924a9b44eee4f189ac00da5b
icovada <federico@tabbo.it>
2024-11-01 18:58:45 +0000

    Open and closed slots

--- od-frontend/src/components/OpeningBlock.vue
+++ od-frontend/src/components/OpeningBlock.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { defineProps } from 'vue'
 import OpenOffice from '@/components/TimetableBlock.vue'
-import { type OutlookEvent } from '@/stores/calendarmanager';
+import { type OutlookEvent } from '@/stores/calendarmanager'
 
 const props = defineProps<{
   outlookevent: Array<OutlookEvent>
@@ -9,10 +9,10 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="container pt-4 px-4">
+  <div class="container pt-4 px-4" v-if="outlookevent.length > 0">
     <h1 class="display-1 d-flex py-3">
       <strong>
-        <slot></slot>
+        <slot name="open"></slot>
       </strong>
     </h1>
     <div class="container">
@@ -20,5 +20,11 @@ const props = defineProps<{
         <OpenOffice :outlookevent="item"></OpenOffice>
       </template>
     </div>
+  </div>
+  <div class="container pt-4 px-4" v-else>
+    <h1 class="display-1 d-flex py-3" style="text-align: center">
+      <strong><slot name="closed"></slot></strong>
+    </h1>
+    <hr />
   </div>
 </template>
--- od-frontend/src/views/HomeView.vue
+++ od-frontend/src/views/HomeView.vue
@@ -1,19 +1,26 @@
 <script setup lang="ts">
 import OpenBlock from '@/components/OpeningBlock.vue'
-import { calendarManager } from '@/stores/calendarmanager';
-import { onMounted } from 'vue';
+import { calendarManager } from '@/stores/calendarmanager'
+import { onMounted } from 'vue'
 
 const store = calendarManager()
 
 onMounted(() => {
   store.getJson()
 })
-
 </script>
 
 <template>
   <main>
-    <OpenBlock :outlookevent="store.today">Uffici ora aperti al pubblico</OpenBlock>
-    <OpenBlock :outlookevent="store.later">Prossime aperture</OpenBlock>
+    <OpenBlock :outlookevent="store.today">
+      <template v-slot:open>Uffici ora aperti al pubblico</template>
+      <template v-slot:closed
+        >Tutti gli uffici sono chiusi al pubblico</template
+      >
+    </OpenBlock>
+    <OpenBlock :outlookevent="store.later">
+      <template v-slot:open>Prossime aperture</template>
+      <template v-slot:closed>Dati temporaneamente non disponibili</template>
+    </OpenBlock>
   </main>
 </template>
