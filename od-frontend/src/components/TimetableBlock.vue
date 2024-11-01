85ad07d9fbbbfce76d541cb1f8a347a7b4868228
icovada <federico@tabbo.it>
2024-11-01 17:58:55 +0000

    Correctly schedule timeouts

--- od-frontend/src/components/TimetableBlock.vue
+++ od-frontend/src/components/TimetableBlock.vue
@@ -39,6 +39,7 @@ function updateOSE() {
       .setLocale('it-IT')
       .toFormat('ccc d LLL, H:mm')
   } else {
+    let millis = 1000
     if (thisevent.end.plus({ minutes: hide_after_min }) < now) {
       //Already over by over 10 min
       ose.value.visible = false
@@ -46,25 +47,55 @@ function updateOSE() {
       //Over
       ose.value.buttontype = 'danger'
       ose.value.text = 'Chiuso'
+
+      //Callback to 10 min after close
+      millis = thisevent.end
+        .plus({ minutes: hide_after_min, seconds: 10 })
+        .diff(now).milliseconds
+      intervalId = setTimeout(updateOSE, millis)
     } else if (thisevent.end.minus({ minutes: warn_before_min }) < now) {
       // About to end
       ose.value.buttontype = 'warning'
       ose.value.text =
         'Chiude tra ' + thisevent.end.diff(now).toFormat('m') + ' minuti'
+
+      // Callback at :00 of the next minute
+      millis = now.endOf('minute').diff(now).milliseconds
+      intervalId = setTimeout(updateOSE, millis)
     } else if (thisevent.begin < now) {
       //open
       ose.value.buttontype = 'success'
       ose.value.text = 'Aperto fino alle ' + thisevent.end.toFormat('H:mm')
-    } else if (thisevent.begin.minus({ minutes: countdown_to_open_min }) < now) {
+
+      // Callback at 14 minutes and 50 seconds before end
+      millis = thisevent.end
+        .minus({ minutes: warn_before_min })
+        .plus({ seconds: 5 })
+        .diff(now).milliseconds
+      intervalId = setTimeout(updateOSE, millis)
+    } else if (
+      thisevent.begin.minus({ minutes: countdown_to_open_min }) < now
+    ) {
       // Hasn't started yet
       ose.value.buttontype = 'primary'
       ose.value.text =
         'Apre tra ' + thisevent.begin.diff(now).toFormat('m:ss') + ' minuti'
-      intervalId = setTimeout(() => updateOSE(), 1000)
+
+      // Callback in 1 second
+      millis = 1000
+      intervalId = setTimeout(() => updateOSE(), millis)
     } else {
       ose.value.buttontype = 'info'
       ose.value.text = 'Apre alle ' + thisevent.begin.toFormat('H:mm')
+
+      // Callback to countdown_to_open min
+      millis = thisevent.begin
+        .minus({ minutes: countdown_to_open_min })
+        .plus({ seconds: 5 })
+        .diff(now).milliseconds
+      intervalId = setTimeout(updateOSE, millis)
     }
+    // console.log('Set timeout for %d millis', millis)
   }
 }
 
