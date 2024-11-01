4d6c931d19401de06e597f2d46fbbbf520807571
icovada <federico@tabbo.it>
2024-11-01 18:14:34 +0000

    test data from HomeView

--- od-frontend/src/components/OpeningBlock.vue
+++ od-frontend/src/components/OpeningBlock.vue
@@ -1,29 +1,11 @@
 <script setup lang="ts">
+import { defineProps } from 'vue'
 import OpenOffice from '@/components/TimetableBlock.vue'
-import { type OutputEvent } from '@/stores/calendarmanager';
-import { DateTime } from 'luxon';
+import { type OutlookEvent } from '@/views/HomeView.vue'
 
-defineProps({
-  slots: Array<OutputEvent>,
-})
-
-export interface OutlookEvent {
-  id: string
-  name: string
-  text: string
-  begin: DateTime,
-  end: DateTime
-}
-
-const ose = {
-  id: "fdsfdsafdas",
-  name: "antani",
-  text: "cacca",
-  begin: DateTime.fromISO('2024-11-01T16:00:50', {zone: 'UTC'}).setZone('CET'),
-  end: DateTime.fromISO('2024-11-01T18:00:00', {zone: 'UTC'}).setZone('CET')
-} as OutlookEvent
-
-
+const props = defineProps<{
+  outlookevent: Array<OutlookEvent>
+}>()
 </script>
 
 <template>
@@ -34,9 +16,9 @@ const ose = {
       </strong>
     </h1>
     <div class="container">
-      <OpenOffice :outlookevent="ose"></OpenOffice>
-      <!-- <template v-for="item in slots" v-bind:key="item.ufficio">
-      </template> -->
+      <template v-for="item in props.outlookevent" v-bind:key="item.ufficio">
+        <OpenOffice :outlookevent="item"></OpenOffice>
+      </template>
     </div>
   </div>
 </template>
--- od-frontend/src/views/HomeView.vue
+++ od-frontend/src/views/HomeView.vue
@@ -1,29 +1,55 @@
 <script setup lang="ts">
-import OpenBlock from '@/components/OpeningBlock.vue';
-import { openOffices } from '@/stores/calendarmanager';
+import OpenBlock from '@/components/OpeningBlock.vue'
+import { DateTime } from 'luxon'
 
-const store = openOffices()
+export interface OutlookEvent {
+  id: string
+  name: string
+  begin: DateTime
+  end: DateTime
+}
 
-// let intervalId: NodeJS.Timeout;
-
-// function getNewJson() {
-//   intervalId = setTimeout(() => getNewJson(), 1000)
-//   store.getJson()
-//   return intervalId
-// }
-
-// onMounted(() => {
-//   intervalId = getNewJson()
-// });
-
-// onBeforeUnmount(() => {
-//   clearInterval(intervalId);
-// })
+const store = {
+  today: [
+    {
+      id: 'fdsfdsafdas',
+      name: 'antani',
+      begin: DateTime.fromISO('2024-11-01T19:55:00', { zone: 'UTC' }).setZone(
+        'CET',
+      ),
+      end: DateTime.fromISO('2024-11-01T22:00:00', { zone: 'UTC' }).setZone(
+        'CET',
+      ),
+    } as OutlookEvent,
+        {
+      id: 'fdsfdsafdas',
+      name: 'sblorfz',
+      begin: DateTime.fromISO('2024-11-01T19:55:00', { zone: 'UTC' }).setZone(
+        'CET',
+      ),
+      end: DateTime.fromISO('2024-11-01T22:00:00', { zone: 'UTC' }).setZone(
+        'CET',
+      ),
+    } as OutlookEvent,
+  ],
+  later:  [
+    {
+      id: 'fdsfdsafdas',
+      name: 'antani',
+      begin: DateTime.fromISO('2024-11-01T19:55:00', { zone: 'UTC' }).setZone(
+        'CET',
+      ),
+      end: DateTime.fromISO('2024-11-01T22:00:00', { zone: 'UTC' }).setZone(
+        'CET',
+      ),
+    } as OutlookEvent,
+  ],
+}
 </script>
 
 <template>
   <main>
-    <OpenBlock :slots=store.today>Uffici ora aperti al pubblico</OpenBlock>
-    <OpenBlock :slots=store.later>Prossime aperture</OpenBlock>
+    <OpenBlock :outlookevent="store.today">Uffici ora aperti al pubblico</OpenBlock>
+    <OpenBlock :outlookevent="store.later">Prossime aperture</OpenBlock>
   </main>
 </template>
