00f6ae5beac07a696ade0f515d55a9373fb0d759
icovada <federico@tabbo.it>
2024-11-02 08:20:53 +0000

    Add logic back

--- od-frontend/src/components/OpeningBlock.vue
+++ od-frontend/src/components/OpeningBlock.vue
@@ -9,22 +9,28 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="container pt-4 px-4">
+  <div
+    class="container pt-4 px-4"
+    v-if="Object.entries(props.outlookevent).length > 0"
+  >
     <h1 class="display-1 d-flex py-3">
       <strong>
         <slot name="open"></slot>
       </strong>
     </h1>
     <div class="container">
-      <template v-for="[key, value] of Object.entries(props.outlookevent)" v-bind:key="key">
+      <template
+        v-for="[key, value] of Object.entries(props.outlookevent)"
+        v-bind:key="key"
+      >
         <OpenOffice :outlookevent="value"></OpenOffice>
       </template>
     </div>
   </div>
-  <!-- <div class="container pt-4 px-4" v-else>
+  <div class="container pt-4 px-4" v-else>
     <h1 class="display-1 d-flex py-3" style="text-align: center">
       <strong><slot name="closed"></slot></strong>
     </h1>
     <hr />
-  </div> -->
+  </div>
 </template>
