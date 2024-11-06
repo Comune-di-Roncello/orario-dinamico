ab7d159331a6d065f31d3bba5c0397b2d7987b10
icovada <federico@tabbo.it>
2024-11-06 12:00:25 +0000

    Add host setup guide

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 Una semplice pagina web in Django per mostrare gli orari degli uffici sullo schermo nella porta del Comune.
 
 Indica gli uffici aperti oggi e l'eventuale apertura successiva nell'arco della prossima settimana
+
+# Installazione
+
+Eseguire `setup_kiosk.sh` per impostare un sistema Debian base per autologin con
+esecuzione automatica di Firefox
+
+
---
+++ setup_kiosk.sh
@@ -0,0 +1,36 @@
+# Script to install and configure a base Debian system to auto-launch
+# Firefox in kiosk mode at login
+
+# Inspiration taken from https://die-antwort.eu/techblog/2017-12-setup-raspberry-pi-for-kiosk-mode/
+
+
+sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox
+sudo apt-get install --no-install-recommends firefox-esr
+sudo apt install nginx
+
+sudo adduser --disabled-password --gecos "" orario
+
+echo """#
+xset s off
+xset s noblank
+xset -dpms
+
+# Allow quitting the X server with CTRL-ATL-Backspace
+setxkbmap -option terminate:ctrl_alt_bksp
+
+DISPLAY=:0 xrandr --output HDMI-1 --rotate left
+
+firefox -kiosk -private-window http://localhost""" | sudo tee /etc/xdg/openbox/autostart
+
+sudo sed -i 's/#NAutoVTs=6/NAutoVTs=1/g' /etc/systemd/logind.conf
+
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
+
+echo """[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin orario --noclear %I 38400 linux""" | sudo tee /etc/systemd/system/getty@tty1.service.d/override.conf
+
+echo "[[ -z \$DISPLAY && \$XDG_VTNR -eq 1 ]] && startx -- -nocursor" | sudo tee /home/orario/.bash_profile
+
+sudo systemctl daemon-reload
+sudo systemctl enable getty@tty1.service
